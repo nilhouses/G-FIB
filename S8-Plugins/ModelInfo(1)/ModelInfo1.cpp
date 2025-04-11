c95b4ddd7cfc1143436794deadf9fac063a5cec1
Nil Casas Duatis <nil.cases@gmail.com>
2025-04-11 17:39:22 +0200

    update

---
+++ S8-Plugins/ModelInfo(1)/ModelInfo1.cpp
@@ -0,0 +1,53 @@
+#include "ModelInfo1.h"
+#include "glwidget.h"
+
+void ModelInfo1::onPluginLoad()
+{
+	
+}
+
+void ModelInfo1::preFrame()
+{
+	
+}
+
+void ModelInfo1::postFrame()
+{
+	//número d'objectes carregats
+
+	//número total de polígons
+	//número total de vèrtexs
+	//percentatge de polígons que són triangles
+
+}
+
+void ModelInfo1::onObjectAdd()
+{
+	
+}
+
+bool ModelInfo1::drawScene()
+{
+	return false; // return true only if implemented
+}
+
+bool ModelInfo1::drawObject(int)
+{
+	return false; // return true only if implemented
+}
+
+bool ModelInfo1::paintGL()
+{
+	return false; // return true only if implemented
+}
+
+void ModelInfo1::keyPressEvent(QKeyEvent *)
+{
+	
+}
+
+void ModelInfo1::mouseMoveEvent(QMouseEvent *)
+{
+	
+}
+
